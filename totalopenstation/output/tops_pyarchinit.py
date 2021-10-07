f2a043180df49dcb19dc5838439d32ed1e192cbe
enzo <enzo.ccc@gmail.com>
2021-10-07 19:58:37 +0200

    update

--- totalopenstation/output/tops_pyarchinit.py
+++ totalopenstation/output/tops_pyarchinit.py
@@ -37,7 +37,7 @@ class OutputFormat(Builder):
     def __init__(self, data):
         self.data = data
         self.output = io.StringIO()
-        fieldnames = ['sito_q', 'area_q', 'us_q','unita_misu_q','quota_q','x','y']
+        fieldnames = ['gid','sito_q', 'area_q', 'us_q','unita_misu_q','quota_q','x','y']
         self.writer = csv.DictWriter(self.output, quoting=csv.QUOTE_NONNUMERIC, fieldnames=fieldnames)
         self.writer.writeheader()
 
@@ -45,7 +45,7 @@ class OutputFormat(Builder):
 
         for feature in self.data:
             row = {
-                #'gid': feature.id,
+                'gid': feature.id,
                 'us_q': feature.desc,
                 'x': feature.geometry.x,
                 'y': feature.geometry.y
