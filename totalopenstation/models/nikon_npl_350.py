4cf3980157d2d7d1ebb51298512e64acd5b23381
enzo <enzo.ccc@gmail.com>
2021-10-11 20:59:34 +0200

    xon/xoff setted false

--- totalopenstation/models/nikon_npl_350.py
+++ totalopenstation/models/nikon_npl_350.py
@@ -26,4 +26,4 @@ from . import *
 class ModelConnector(Connector):
 
     def __init__(self, port):
-        Connector.__init__(self, port=port, baudrate=1200, xonxoff=True)
+        Connector.__init__(self, port=port, baudrate=1200, xonxoff=False)
